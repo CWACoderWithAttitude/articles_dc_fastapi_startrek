d70a3141c19afbb92ea81eff97e737ac01d167e5
Volker Benders <39129892+CWACoderWithAttitude@users.noreply.github.com>
2025-04-18 21:05:26 +0000

    ignore main-block from coverage

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 async def root() -> dict[str, str]:
     return {"message": "Welcome to the FastAPI application!"}
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     import uvicorn
 
     uvicorn.run(app=app, host="0.0.0.0", port=8000)
