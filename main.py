1b53acd459e1c53ca7ef8b5819eb18e0860d211e
Volker Benders <39129892+CWACoderWithAttitude@users.noreply.github.com>
2025-04-21 17:14:46 +0000

    add metrics exporter to app

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from typing import Any, Generator
 from fastapi import FastAPI, HTTPException
 from fastapi import Depends, status
+from prometheus_fastapi_instrumentator import Instrumentator
 from sqlmodel import Field, SQLModel, Session, create_engine, select
 from routers.ships_router import router as ships_router
 
 
-
 app = FastAPI()
+
+Instrumentator().instrument(app).expose(app)
 app.include_router(ships_router)
+
 
 @app.get("/", tags=["root"])
 async def root() -> dict[str, str]:
