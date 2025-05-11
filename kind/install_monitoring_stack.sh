0d1df7d23376c6cb8ae749a91da3b0ee3f5f5a49
Volker Benders <39129892+CWACoderWithAttitude@users.noreply.github.com>
2025-05-11 20:44:18 +0200

    wip

---
+++ kind/install_monitoring_stack.sh
@@ -0,0 +1,27 @@
+#/!/bin/sh
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add stable https://charts.helm.sh/stable
+helm repo update
+kubectl create namespace monitoring
+#helm install kind-prometheus prometheus-community/kube-prometheus-stack \
+#  --namespace monitoring \
+#  --set prometheus.service.nodePort=30000 \
+#  --set prometheus.service.type=NodePort \
+#  --set grafana.service.nodePort=31000 \
+# --set grafana.service.type=NodePort \
+#  --set alertmanager.service.nodePort=32000 \
+#  --set alertmanager.service.type=NodePort \
+#  --set prometheus-node-exporter.service.nodePort=32001 \
+#  --set prometheus-node-exporter.service.type=NodePort
+
+# verify the installation
+kubectl get namespaces
+kubectl get svc -n monitoring
+
+# Add labels to nodes for easier identification
+kubectl label nodes kind-worker node=worker1
+kubectl label nodes kind-worker2 node=worker2
+kubectl get nodes --show-labels
+
+
